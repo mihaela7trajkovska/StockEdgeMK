e8104feb3a8f51e7ffce37fb02ef00acdc370628
frontend <elenacikarska50@gmail.com>
2025-01-19 21:48:38 +0100

    Додадени видеа

--- Domasnaa_2/Prototip/backend/StockEdgeMK/src/main/java/mk/ukim/finki/stockedgemk/repository/StockRepository.java
+++
@@ -1,9 +0,0 @@
-package mk.ukim.finki.stockedgemk.repository;
-import mk.ukim.finki.stockedgemk.model.Stock;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.Optional;
-
-public interface StockRepository extends JpaRepository<Stock, Long> {
-    Optional<Stock> findByTicker(String ticker);
-}
--- README.md
+++
@@ -1,9 +0,0 @@
-StockEdgeMK
-
-Team for this project:
-
-Elena Cikarska 221122
-
-Vikorija Serafimovska 221139
-
-Mihaela Trajkovska 221166
